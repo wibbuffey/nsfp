04041d3ba09871d18971f39a4cbd47b67bf8c32e
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-10-18 11:14:48 -0400

    Rename parse.js to main.js and minify

---
+++ main.js
@@ -0,0 +1,1 @@
+module.exports=(a,r)=>{f=[],o=[];a.map(e=>{b=e.slice.bind(e);c=b(1);g=f.push.bind(f);h=e.startsWith.bind(e);h("--")?(()=>g(b(2)))():(()=>{h("-")?(()=>{d=r[c];d?g(d):g(c)})():o.push(e)})()});return[f,o]}
