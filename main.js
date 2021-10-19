926619166c532de32186eb9dbf841b0e2e4bd36d
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-10-18 22:43:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ module.exports = (a, r = {}, i = []) => {
     k = i.indexOf(j);
     h = e.startsWith.bind(e);
     h("--") ? (() => {
-      k < 0 ? f[j] = 1 : f[j] = a[k + 1]
+      k < 0 ? f[j] = 1 : f[j] = a[k + 2]
     })() : (() => {
       h("-") ? (() => {
         d = r[c];
         d ? f[d] = 1 : f[c] = 1
       })() : o.push(e)
     })()
-  })
+  });
   return [f, o]
 }
