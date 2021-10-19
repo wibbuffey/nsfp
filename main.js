b239f9b536180bb69f801cbc315fbcaa82f8802f
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-10-18 22:32:57 -0400

    add support for arguments that take an option

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-module.exports = (a, r) => {
-  f = [], o = [];
+module.exports = (a, r = {}, i = []) => {
+  f = {}, o = [];
   a.map(e => {
     b = e.slice.bind(e);
     c = b(1);
-    g = f.push.bind(f);
+    j = b(2);
+    k = i.indexOf(j);
     h = e.startsWith.bind(e);
-    h("--") ? (
-      () => g(b(2))
-    )() : (() => {
+    h("--") ? (() => {
+      k < 0 ? f[j] = 1 : f[j] = a[k + 1]
+    })() : (() => {
       h("-") ? (() => {
-        d = r[c];
-        d ? g(d) : g(c)
+        d = r[c]
+        d ? (() => f[d] = 1)() : (() => f[c] = 1)()
       })() : o.push(e)
     })()
   });
